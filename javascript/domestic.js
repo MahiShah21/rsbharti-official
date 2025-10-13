0e6443ab803fc19e139ba358c1708cf36a474b64
MahiShah21 <mahishah21906@gmail.com>
2025-10-13 15:14:52 +0530

    hindustan uniliver removed

--- javascript/domestic.js
+++ javascript/domestic.js
@@ -4,10 +4,6 @@ const data = [
     title: "BRITANNIA",
     text: "For over 130 years, Britannia has been a cherished part of life for millions of families across India and the world. Since our humble beginnings in 1892, we've grown into one of India's leading and most trusted food companies, committed to the promise of 'Eat Healthy, Think Better.' Our journey is one built on a legacy of quality, innovation, and an unwavering commitment to bringing delicious and wholesome food to your table. From the iconic crunch of Marie Gold to the melt-in-your-mouth goodness of Good Day cookies and our trusted range of breads and dairy products, we craft every product with care, ensuring that every bite is a moment of delight and nutrition."  },
   {
-    img: "images/unilever.png",
-    title: "UNILEVER",
-    text: "Every day, you interact with a Unilever brand. Our expansive and iconic portfolio covers four main business groups: Beauty & Wellbeing, Personal Care, Home Care, and Nutrition. We aim to meet your everyday needs and help you feel good, look good, and get more out of life. Wake up with the freshness of a Dove or Lux soap, enjoy a delicious meal made with Knorr, or clean your home with trusted names like Domestos. From the irresistible pleasure of Magnum to the vital hydration of Liquid I.V., Unilever's brands are committed to quality, innovation, and enriching your daily routine. " },
-  {
     img: "images/pynk.png",
     title: "PYNK",
     text: "Pynk is the dynamic women's wear brand from the trusted house of Lux Industries, crafted for the modern Indian woman. Drawing on a legacy of quality and comfort, Pynk is not just clothing; it is a mindset of fluid femininity that celebrates a woman's journey, resilience, and pride. Our collection seamlessly blends comfort, contemporary design, and confidence across a wide range of apparel. We are committed to making fashionable and high-quality women's wear accessible across the country, ensuring that every woman can move freely and express herself unapologetically, feeling both comfortable and confident, every single day"  },
